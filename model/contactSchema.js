05182cd39cf61b7b46d8dd022b131da3c575ddaa
Brandy CAYPE <brandycype234@gmail.com>
2022-12-08 23:38:20 +0300

    db connection

---
+++ model/contactSchema.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const contactSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
+});
+const Contact=mongoose.model('contacts',contactSchema);
+module.exports=Contact;
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express')
-require('dotenv').config()
-const app=express()
-const fs=require('fs')
+const express = require('express');
+require('dotenv').config();
+const app=express();
+const fs=require('fs');
 const port=process.env.PORT || 5000
+const mongoose=require('mongoose');
+const contactModel=require('./model/contactSchema')
+const dbURI= 'mongodb+srv://<username>:<password>@cluster-name.26v6e6t.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+.then(()=>{
+    app.listen(port,()=>{
 
-app.listen(port,()=>{
-
-    console.log(`Server running at :http://localhost:${port}`);
+        console.log(`Server running at :http://localhost:${port}`)
+    });
+    console.log('Db connected');
 })
+.catch((err)=>{
+    console.log(err);
+})
+
 app.set('view engine','ejs')
 
 app.use(express.urlencoded({extended:true}))
@@ -27,10 +37,16 @@ app.get('/contact',(req,res)=>{
 })
 app.post('/contact',(req,res)=>{
     const data =req.body
-
+    const message = new contactModel(data)
+    message.save()
+    .then((result)=>{
+        console.log(result);
+    })
+.catch((err)=>{
+    console.log(err);
+})
      fs.writeFileSync('contact.txt',JSON.stringify(data))
    console.log(data);
 
 
-    res.end()
 })
